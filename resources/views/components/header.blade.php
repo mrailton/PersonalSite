45bd1e417f175dba41b732e9db9abdbb44d80042
Mark Railton <marksrailton@gmail.com>
2022-12-12 19:38:10 +0000

    Adds fonts and tidies index

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
     <nav class="border-b">
         <div x-data="{showMenu : false}" class="container max-w-screen-2xl mx-auto flex justify-between h-14 sm:text-gray-700 md:text-white">
             <div class="flex items-center text-white mr-6">
-                <a class="flex items-center text-white" href="{{ route('index') }}">
+                <a class="flex items-center text-white pl-4 " href="{{ route('index') }}">
                     <svg class="h-6 w-6 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
