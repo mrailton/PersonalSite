4dd2f73529a76f5947fd25013a0987d8c972698a
Mark Railton <marksrailton@gmail.com>
2022-12-13 12:20:12 +0000

    Fix tests

--- tests/Feature/Articles/ListArticlesTest.php
+++ tests/Feature/Articles/ListArticlesTest.php
@@ -12,7 +12,7 @@ class ListArticlesTest extends TestCase
     /** @test */
     public function it_displays_a_list_of_published_articles(): void
     {
-        $publishedArticles = Article::factory()->count(12)->create();
+        $publishedArticles = Article::factory()->count(4)->create();
         $unpublishedArticles = Article::factory()->unpublished()->count(2)->create();
 
         $res = $this->get(route('articles.list'));
@@ -22,6 +22,18 @@ class ListArticlesTest extends TestCase
             ->assertSee($publishedArticles[2]->title)
             ->assertDontSee($unpublishedArticles[0]->title)
             ->assertDontSee($unpublishedArticles[1]->title)
+            ->assertDontSee('Newer')
+            ->assertDontSee('Older');
+    }
+
+    /** @test */
+    public function it_displays_pagination_when_more_then_10_posts_are_published(): void
+    {
+        $publishedArticles = Article::factory()->count(12)->create();
+
+        $res = $this->get(route('articles.list'));
+
+        $res->assertSee('Blog Articles')
             ->assertSee('Newer')
             ->assertSee('Older');
     }
