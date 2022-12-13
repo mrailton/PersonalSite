6295f88b7ec6aec22ea75fb24712615823ef5aa2
Mark Railton <marksrailton@gmail.com>
2022-12-13 12:40:13 +0000

    Add admin to vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/admin.css',
+                'resources/css/app.css',
+                'resources/js/admin.js',
+                'resources/js/app.js'
+            ],
             refresh: true,
             valetTls: 'personalsite.test',
         }),
