0b5898c23cd621cce80882f1847620562ad25d22
Mark Railton <marksrailton@gmail.com>
2022-12-16 09:18:00 +0000

    Add shell to hljs

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,13 @@ import php from 'highlight.js/lib/languages/php';
 import javascript from 'highlight.js/lib/languages/javascript';
 import css from 'highlight.js/lib/languages/css';
 import python from 'highlight.js/lib/languages/python';
+import shell from 'highlight.js/lib/languages/shell';
 
 hljs.registerLanguage('php', php);
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('css', css);
 hljs.registerLanguage('python', python);
+hljs.registerLanguage('shell', shell);
 
 hljs.highlightAll();
 
