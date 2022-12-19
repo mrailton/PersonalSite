b5ee70dcc34fc7cdcd85b2c1e51ea6fb5bf88bd1
Mark Railton <marksrailton@gmail.com>
2022-12-19 19:26:16 +0000

    reorg head a lil

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="/img/favicon/favicon.ico">
-    <title>Mark Railton</title>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @production
         <script defer data-domain="markrailton.com" src="https://analytics.markrailton.com/js/plausible.js"></script>
     @endproduction
+
+    <link rel="shortcut icon" href="/img/favicon/favicon.ico">
+    <title>Mark Railton</title>
 </head>
 <body>
 <x-header />
