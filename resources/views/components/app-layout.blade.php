43a5b3ec35fe359dd417b3c1920ed02b5a0fd12d
Mark Railton <marksrailton@gmail.com>
2022-12-20 18:11:42 +0000

    Moved ackee

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -8,7 +8,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @production
-        <script async src="https://analytics.markrailton.com/tracker.js" data-ackee-server="https://analytics.markrailton.com" data-ackee-domain-id="43f9ae3a-584e-4d9b-a6fa-2657e59c2c21"></script>
+        <script async src="https://analytics.markrailton.com/tracker.js" data-ackee-server="https://analytics.markrailton.com" data-ackee-domain-id="22521880-24bb-471a-9620-20e6d2263018"></script>
     @endproduction
 
     <link rel="shortcut icon" href="/img/favicon/favicon.ico">
