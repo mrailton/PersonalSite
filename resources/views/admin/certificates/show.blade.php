25520ba131cede5f85d98e08a11de6c85723c95c
Mark Railton <marksrailton@gmail.com>
2023-02-10 20:09:29 +0000

    Adds ability to update a certificate
    TODO: need to handle updating file

--- resources/views/admin/certificates/show.blade.php
+++ resources/views/admin/certificates/show.blade.php
@@ -1,4 +1,14 @@
 <x-admin-layout title="Show Certificate">
+    <div class="sm:flex sm:items-center py-5 px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex-auto">
+
+        </div>
+        <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                <a href="{{ route('admin.certificates.edit', ['certificate' => $certificate]) }}">
+                    <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Update Certificate</button>
+                </a>
+        </div>
+    </div>
     <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
         <dl class="sm:divide-y sm:divide-gray-200">
             <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
