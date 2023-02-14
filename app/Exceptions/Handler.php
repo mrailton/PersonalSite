c92cf47877ed64d6d046e388cd47c175800820be
Shift <shift@laravelshift.com>
2023-02-14 16:32:01 +0000

    Remove redundant typing from DocBlocks

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -40,8 +40,6 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
-     *
-     * @return void
      */
     public function register(): void
     {
