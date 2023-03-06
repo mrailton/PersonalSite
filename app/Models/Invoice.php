2861a1e70bd671494255c61bd6371de2ac3f31d7
Mark Railton <marksrailton@gmail.com>
2023-03-06 16:14:34 +0000

    Allow invoices to list if customer has been deleted

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -27,7 +27,7 @@ class Invoice extends Model
 
     public function customer(): BelongsTo
     {
-        return $this->belongsTo(Customer::class, 'customer_id', 'id');
+        return $this->belongsTo(Customer::class, 'customer_id', 'id')->withTrashed();
     }
 
     public function items(): HasMany
