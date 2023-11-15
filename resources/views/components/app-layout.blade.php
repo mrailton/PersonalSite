a2ae545f21f161466d498a7156e743623d7a0817
Mark Railton <marksrailton@gmail.com>
2023-11-15 17:42:19 +0000

    Remove analytics

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -11,24 +11,6 @@
     <link rel="me" href="https://phpc.social/@markrailton">
     <title>Mark Railton</title>
 
-    @production
-        <!-- Matomo -->
-        <script>
-            var _paq = window._paq = window._paq || [];
-            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-                var u="//analytics.markrailton.com/";
-                _paq.push(['setTrackerUrl', u+'matomo.php']);
-                _paq.push(['setSiteId', '1']);
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-        </script>
-        <!-- End Matomo Code -->
-    @endproduction
-
     {{ $head ?? '' }}
 </head>
 <body>
