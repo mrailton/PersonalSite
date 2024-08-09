17567cb4f82cf3616cd5bf8c82bc9994015b8c25
Shift <shift@laravelshift.com>
2024-08-09 10:07:38 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,8 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public const HOME = '/admin';
+
     /**
      * Register any application services.
      */
